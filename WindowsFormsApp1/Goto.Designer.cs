c35199804c4863d87ca08ed2eef0a34cbc8a09b0
johnny263 <63039021+johnny263@users.noreply.github.com>
2024-01-21 16:12:13 +0800

    visual change

--- WindowsFormsApp1/Goto.Designer.cs
+++ WindowsFormsApp1/Goto.Designer.cs
@@ -20,140 +20,137 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			OptionLabel = new System.Windows.Forms.Label();
-			GotoInput = new System.Windows.Forms.TextBox();
-			GoButton = new System.Windows.Forms.Button();
-			CancelGotoButton = new System.Windows.Forms.Button();
-			OptionLine = new System.Windows.Forms.RadioButton();
-			OptionPos = new System.Windows.Forms.RadioButton();
-			OptionsGroupBox = new System.Windows.Forms.GroupBox();
-			IgnoreOverflowCheckBox = new System.Windows.Forms.CheckBox();
-			OptionsGroupBox.SuspendLayout();
-			SuspendLayout();
-			// 
-			// OptionLabel
-			// 
-			OptionLabel.AutoSize = true;
-			OptionLabel.Location = new System.Drawing.Point(14, 23);
-			OptionLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-			OptionLabel.Name = "OptionLabel";
-			OptionLabel.Size = new System.Drawing.Size(36, 15);
-			OptionLabel.TabIndex = 0;
-			OptionLabel.Text = "Goto:";
-			// 
-			// GotoInput
-			// 
-			GotoInput.Location = new System.Drawing.Point(30, 76);
-			GotoInput.Margin = new System.Windows.Forms.Padding(2);
-			GotoInput.Name = "GotoInput";
-			GotoInput.Size = new System.Drawing.Size(318, 23);
-			GotoInput.TabIndex = 1;
-			GotoInput.KeyPress += GotoInputEvent;
-			// 
-			// GoButton
-			// 
-			GoButton.Location = new System.Drawing.Point(83, 114);
-			GoButton.Margin = new System.Windows.Forms.Padding(2);
-			GoButton.Name = "GoButton";
-			GoButton.Size = new System.Drawing.Size(73, 23);
-			GoButton.TabIndex = 2;
-			GoButton.Text = "Go";
-			GoButton.UseVisualStyleBackColor = true;
-			GoButton.Click += Go_Click;
-			// 
-			// CancelGotoButton
-			// 
-			CancelGotoButton.Location = new System.Drawing.Point(211, 114);
-			CancelGotoButton.Margin = new System.Windows.Forms.Padding(2);
-			CancelGotoButton.Name = "CancelGotoButton";
-			CancelGotoButton.Size = new System.Drawing.Size(73, 23);
-			CancelGotoButton.TabIndex = 3;
-			CancelGotoButton.Text = "Cancel";
-			CancelGotoButton.UseVisualStyleBackColor = true;
-			CancelGotoButton.Click += Cancel_Click;
-			// 
-			// OptionLine
-			// 
-			OptionLine.AutoSize = true;
-			OptionLine.Checked = true;
-			OptionLine.Location = new System.Drawing.Point(53, 21);
-			OptionLine.Margin = new System.Windows.Forms.Padding(2);
-			OptionLine.Name = "OptionLine";
-			OptionLine.Size = new System.Drawing.Size(44, 19);
-			OptionLine.TabIndex = 4;
-			OptionLine.TabStop = true;
-			OptionLine.Text = "line";
-			OptionLine.UseVisualStyleBackColor = true;
-			OptionLine.Click += OptionLine_Click;
-			// 
-			// OptionPos
-			// 
-			OptionPos.AutoSize = true;
-			OptionPos.Location = new System.Drawing.Point(101, 21);
-			OptionPos.Margin = new System.Windows.Forms.Padding(2);
-			OptionPos.Name = "OptionPos";
-			OptionPos.Size = new System.Drawing.Size(44, 19);
-			OptionPos.TabIndex = 5;
-			OptionPos.TabStop = true;
-			OptionPos.Text = "pos";
-			OptionPos.UseVisualStyleBackColor = true;
-			OptionPos.Click += OptionPos_Click;
-			// 
-			// OptionsGroupBox
-			// 
-			OptionsGroupBox.Controls.Add(IgnoreOverflowCheckBox);
-			OptionsGroupBox.Controls.Add(OptionLabel);
-			OptionsGroupBox.Controls.Add(OptionPos);
-			OptionsGroupBox.Controls.Add(OptionLine);
-			OptionsGroupBox.Location = new System.Drawing.Point(30, 6);
-			OptionsGroupBox.Name = "OptionsGroupBox";
-			OptionsGroupBox.Size = new System.Drawing.Size(200, 65);
-			OptionsGroupBox.TabIndex = 6;
-			OptionsGroupBox.TabStop = false;
-			OptionsGroupBox.Text = "Options";
-			// 
-			// IgnoreOverflowCheckBox
-			// 
-			IgnoreOverflowCheckBox.AutoSize = true;
-			IgnoreOverflowCheckBox.Location = new System.Drawing.Point(53, 40);
-			IgnoreOverflowCheckBox.Name = "IgnoreOverflowCheckBox";
-			IgnoreOverflowCheckBox.Size = new System.Drawing.Size(111, 19);
-			IgnoreOverflowCheckBox.TabIndex = 6;
-			IgnoreOverflowCheckBox.Text = "Ignore Overflow";
-			IgnoreOverflowCheckBox.UseVisualStyleBackColor = true;
-			// 
-			// Goto
-			// 
-			AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-			AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			ClientSize = new System.Drawing.Size(377, 148);
-			Controls.Add(OptionsGroupBox);
-			Controls.Add(CancelGotoButton);
-			Controls.Add(GoButton);
-			Controls.Add(GotoInput);
-			FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-			Margin = new System.Windows.Forms.Padding(2);
-			MaximizeBox = false;
-			MinimizeBox = false;
-			Name = "Goto";
-			Text = "Goto";
-			OptionsGroupBox.ResumeLayout(false);
-			OptionsGroupBox.PerformLayout();
-			ResumeLayout(false);
-			PerformLayout();
-		}
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            OptionLabel = new System.Windows.Forms.Label();
+            GotoInput = new System.Windows.Forms.TextBox();
+            GoButton = new System.Windows.Forms.Button();
+            CancelGotoButton = new System.Windows.Forms.Button();
+            OptionLine = new System.Windows.Forms.RadioButton();
+            OptionPos = new System.Windows.Forms.RadioButton();
+            OptionsGroupBox = new System.Windows.Forms.GroupBox();
+            IgnoreOverflowCheckBox = new System.Windows.Forms.CheckBox();
+            OptionsGroupBox.SuspendLayout();
+            SuspendLayout();
+            // 
+            // OptionLabel
+            // 
+            OptionLabel.AutoSize = true;
+            OptionLabel.Location = new System.Drawing.Point(18, 29);
+            OptionLabel.Name = "OptionLabel";
+            OptionLabel.Size = new System.Drawing.Size(46, 19);
+            OptionLabel.TabIndex = 0;
+            OptionLabel.Text = "Goto:";
+            // 
+            // GotoInput
+            // 
+            GotoInput.Location = new System.Drawing.Point(39, 111);
+            GotoInput.Name = "GotoInput";
+            GotoInput.Size = new System.Drawing.Size(408, 27);
+            GotoInput.TabIndex = 1;
+            GotoInput.KeyPress += GotoInputEvent;
+            // 
+            // GoButton
+            // 
+            GoButton.Location = new System.Drawing.Point(107, 144);
+            GoButton.Name = "GoButton";
+            GoButton.Size = new System.Drawing.Size(94, 29);
+            GoButton.TabIndex = 2;
+            GoButton.Text = "Go";
+            GoButton.UseVisualStyleBackColor = true;
+            GoButton.Click += Go_Click;
+            // 
+            // CancelGotoButton
+            // 
+            CancelGotoButton.Location = new System.Drawing.Point(271, 144);
+            CancelGotoButton.Name = "CancelGotoButton";
+            CancelGotoButton.Size = new System.Drawing.Size(94, 29);
+            CancelGotoButton.TabIndex = 3;
+            CancelGotoButton.Text = "Cancel";
+            CancelGotoButton.UseVisualStyleBackColor = true;
+            CancelGotoButton.Click += Cancel_Click;
+            // 
+            // OptionLine
+            // 
+            OptionLine.AutoSize = true;
+            OptionLine.Checked = true;
+            OptionLine.Location = new System.Drawing.Point(68, 27);
+            OptionLine.Name = "OptionLine";
+            OptionLine.Size = new System.Drawing.Size(55, 23);
+            OptionLine.TabIndex = 4;
+            OptionLine.TabStop = true;
+            OptionLine.Text = "line";
+            OptionLine.UseVisualStyleBackColor = true;
+            OptionLine.Click += OptionLine_Click;
+            // 
+            // OptionPos
+            // 
+            OptionPos.AutoSize = true;
+            OptionPos.Location = new System.Drawing.Point(130, 27);
+            OptionPos.Name = "OptionPos";
+            OptionPos.Size = new System.Drawing.Size(55, 23);
+            OptionPos.TabIndex = 5;
+            OptionPos.TabStop = true;
+            OptionPos.Text = "pos";
+            OptionPos.UseVisualStyleBackColor = true;
+            OptionPos.Click += OptionPos_Click;
+            // 
+            // OptionsGroupBox
+            // 
+            OptionsGroupBox.Controls.Add(IgnoreOverflowCheckBox);
+            OptionsGroupBox.Controls.Add(OptionLabel);
+            OptionsGroupBox.Controls.Add(OptionPos);
+            OptionsGroupBox.Controls.Add(OptionLine);
+            OptionsGroupBox.Location = new System.Drawing.Point(39, 8);
+            OptionsGroupBox.Margin = new System.Windows.Forms.Padding(4);
+            OptionsGroupBox.Name = "OptionsGroupBox";
+            OptionsGroupBox.Padding = new System.Windows.Forms.Padding(4);
+            OptionsGroupBox.Size = new System.Drawing.Size(257, 96);
+            OptionsGroupBox.TabIndex = 6;
+            OptionsGroupBox.TabStop = false;
+            OptionsGroupBox.Text = "Options";
+            // 
+            // IgnoreOverflowCheckBox
+            // 
+            IgnoreOverflowCheckBox.AutoSize = true;
+            IgnoreOverflowCheckBox.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            IgnoreOverflowCheckBox.Location = new System.Drawing.Point(18, 57);
+            IgnoreOverflowCheckBox.Margin = new System.Windows.Forms.Padding(4);
+            IgnoreOverflowCheckBox.Name = "IgnoreOverflowCheckBox";
+            IgnoreOverflowCheckBox.Size = new System.Drawing.Size(148, 23);
+            IgnoreOverflowCheckBox.TabIndex = 6;
+            IgnoreOverflowCheckBox.Text = "Ignore Overflow:";
+            IgnoreOverflowCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // Goto
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(485, 187);
+            Controls.Add(OptionsGroupBox);
+            Controls.Add(CancelGotoButton);
+            Controls.Add(GoButton);
+            Controls.Add(GotoInput);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Goto";
+            Text = "Goto";
+            OptionsGroupBox.ResumeLayout(false);
+            OptionsGroupBox.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
+        #endregion
 
-		private System.Windows.Forms.Label OptionLabel;
+        private System.Windows.Forms.Label OptionLabel;
 		private System.Windows.Forms.Button CancelGotoButton;
 		public System.Windows.Forms.Button GoButton;
 		public System.Windows.Forms.TextBox GotoInput;
--- WindowsFormsApp1/Goto.cs
+++ WindowsFormsApp1/Goto.cs
@@ -13,78 +13,78 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 namespace NotePadMinusMinus
 {
 
-	public partial class Goto : Form
-	{
-		private MainForm _mainForm;
-		public Goto(MainForm form1)
-		{
-			InitializeComponent();
-			_mainForm = form1;
-		}
+    public partial class Goto : Form
+    {
+        private MainForm _mainForm;
+        public Goto(MainForm form1)
+        {
+            InitializeComponent();
+            _mainForm = form1;
+        }
 
-		private void Cancel_Click(object sender, EventArgs e)
-		{
-			this.Close();
-		}
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
 
-		private void Go_Click(object sender, EventArgs e)
-		{
-			if (OptionLine.Checked == true)
-			{
-				// TryParse returns false if failed
-				if (!int.TryParse(GotoInput.Text, out int lineNumber)) lineNumber = int.MaxValue;
-				lineNumber--;
+        private void Go_Click(object sender, EventArgs e)
+        {
+            if (OptionLine.Checked == true)
+            {
+                // TryParse returns false if failed
+                if (!int.TryParse(GotoInput.Text, out int lineNumber)) lineNumber = int.MaxValue;
+                lineNumber--;
 
-				int totalLines = _mainForm.EditingArea.Lines.Length - 1;
-				if (lineNumber > totalLines && !IgnoreOverflowCheckBox.Checked)
-				{
-					MessageBox.Show("The line you wanted to go is over the document!", "Error", MessageBoxButtons.OK);
-					return;
-				}
+                int totalLines = _mainForm.EditingArea.Lines.Length - 1;
+                if (lineNumber > totalLines && !IgnoreOverflowCheckBox.Checked)
+                {
+                    MessageBox.Show("The line you wanted to go is over the document!", "Error", MessageBoxButtons.OK);
+                    return;
+                }
 
-				int lineStartIndex = _mainForm.EditingArea.GetFirstCharIndexFromLine(Utils.Range(0, lineNumber, totalLines));
-				_mainForm.EditingArea.SelectionStart = lineStartIndex;
-				_mainForm.EditingArea.SelectionLength = 0;
-				_mainForm.EditingArea.ScrollToCaret();
-			}
-			else if (OptionPos.Checked == true) // add readability
-			{
-				if (!int.TryParse(GotoInput.Text, out int charIndex)) charIndex = int.MaxValue;
-				charIndex--;
+                int lineStartIndex = _mainForm.EditingArea.GetFirstCharIndexFromLine(Utils.Range(0, lineNumber, totalLines));
+                _mainForm.EditingArea.SelectionStart = lineStartIndex;
+                _mainForm.EditingArea.SelectionLength = 0;
+                _mainForm.EditingArea.ScrollToCaret();
+            }
+            else if (OptionPos.Checked == true) // add readability
+            {
+                if (!int.TryParse(GotoInput.Text, out int charIndex)) charIndex = int.MaxValue;
+                charIndex--;
 
-				int length = _mainForm.EditingArea.Text.Length - 1;
+                int length = _mainForm.EditingArea.Text.Length - 1;
 
-				_mainForm.EditingArea.SelectionStart = Utils.Range(0, charIndex, length);
-				_mainForm.EditingArea.SelectionLength = 0;
-				_mainForm.EditingArea.ScrollToCaret();
-			}
-		}
+                _mainForm.EditingArea.SelectionStart = Utils.Range(0, charIndex, length);
+                _mainForm.EditingArea.SelectionLength = 0;
+                _mainForm.EditingArea.ScrollToCaret();
+            }
+        }
 
-		private void GotoInputEvent(object sender, KeyPressEventArgs e)
-		{
-			if (!char.IsDigit(e.KeyChar) && e.KeyChar != '\b')
-			{
-				e.Handled = true; // Suppress the key press
-			}
-		}
+        private void GotoInputEvent(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != '\b')
+            {
+                e.Handled = true; // Suppress the key press
+            }
+        }
 
-		private void OptionLine_Click(object sender, EventArgs e)
-		{
-			if (OptionLine.Checked == false)
-			{
-				OptionLine.Checked = true;
-				OptionPos.Checked = false;
-			}
-		}
+        private void OptionLine_Click(object sender, EventArgs e)
+        {
+            if (OptionLine.Checked == false)
+            {
+                OptionLine.Checked = true;
+                OptionPos.Checked = false;
+            }
+        }
 
-		private void OptionPos_Click(object sender, EventArgs e)
-		{
-			if (OptionPos.Checked == false)
-			{
-				OptionPos.Checked = true;
-				OptionLine.Checked = false;
-			}
-		}
+        private void OptionPos_Click(object sender, EventArgs e)
+        {
+            if (OptionPos.Checked == false)
+            {
+                OptionPos.Checked = true;
+                OptionLine.Checked = false;
+            }
+        }
 
-	}
+    }
 }
